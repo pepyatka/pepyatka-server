d65008b67a6423c8467668475c6bff5d39f3c394
epicmonkey <mail@epicmonkey.org>
2013-01-13 00:09:38 +0400

    Updates configuration of websocket by explicitly setting production options.

--- io.js
+++ io.js
@@ -4,7 +4,27 @@ var models = require('./app/models')
 
 exports.listen = function(server, connections) {
   var io = require('socket.io').listen(server)
-  
+
+  io.configure('production', function(){
+    io.enable('browser client etag');
+    io.enable('browser client minification');
+    io.enable('browser client gzip');
+
+    io.set('log level', 1);
+
+    io.set('transports', [
+        'websocket'
+      , 'flashsocket'
+      , 'htmlfile'
+      , 'xhr-polling'
+      , 'jsonp-polling'
+    ]);
+  });
+
+  io.configure('development', function(){
+    io.set('transports', ['websocket']);
+  });
+
   io.sockets.on(
     'connection',
 
