76d3bd0e26abb307a5088513fdc55aaff8d9571b
epicmonkey <mail@epicmonkey.org>
2013-01-14 12:11:02 +0400

    Checks if user exists before creating a new one.

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,15 +5,22 @@ exports.addRoutes = function(app) {
     res.render('users/signup');
   });
 
-  app.post('/signup', function(req, res){
-    var newUser = new models.User({
+  app.post('/signup', function(req, res) {
+    var newUser = new models.User( {
       username: req.body.username,
       password: req.body.password
     })
-    newUser.save(function(user) {
-      req.session.userId = user.id
 
-      res.redirect('/')
+    models.User.findByUsername(newUser.username, function(user) {
+      if (user == null) {
+        newUser.save(function(user) {
+          req.session.userId = user.id
+          
+          res.redirect('/')
+        })
+      } else {
+        res.redirect('/signup')
+      }
     })
   });
 }
