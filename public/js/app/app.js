2beb1e139d3d2795351f1740c8cf85f44cbd7bc1
epicmonkey <mail@epicmonkey.org>
2013-01-16 19:25:44 +0400

    Adds New comment link just below all comments if user has participated in the discussion.

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -116,6 +116,41 @@ App.CommentPostView = Ember.View.extend(Ember.TargetActionSupport, {
   }
 })
 
+App.CommentPostViewSubst = Ember.View.extend(Ember.TargetActionSupport, {
+  classNameBindings: 'isVisible visible:invisible',
+
+  click: function() {
+    this.triggerAction();
+  },
+
+  // XXX: this is a dup of App.PostContainerView.toggleVisibility()
+  // function. I just do not know how to access it from UI bindings
+  toggleVisibility: function() {
+    this.toggleProperty('parentView.isFormVisible');
+  },
+
+  // this method does not observe post comments as a result it won't
+  // display additional Add comment link if user does not refresh the page
+  isVisible: function() {
+    var post = this.get('parentView.content')
+    var comments = post.comments
+
+    // return false if comments do not include current user
+    // var exist = post.createdBy.id == currentUser
+    var exist = false
+    comments.forEach(function(comment) {
+      exist = exist || comment.createdBy.id == currentUser
+    })
+
+    // If user have not commented this post there is no need to
+    // display additional comment link at the bottom of the post.
+    if (!exist)
+      return false
+
+    return this.get('parentView.isFormVisible') == false;
+  }.property('parentView.isFormVisible'),
+})
+
 // Text field to post a comment. Separate view to make it hideable
 App.CommentForm = Ember.View.extend({
   // I'd no success to use isVisibleBinding property...
