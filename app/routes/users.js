3d9495accd8fb304bc999b6d597d38a4b7c46e00
epicmonkey <mail@epicmonkey.org>
2013-02-06 01:38:45 +0400

    Adds generic users route for auth users.

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,15 @@
 var models = require('../models');
 
 exports.addRoutes = function(app) {
+  app.get('/v1/users', function(req, res) {
+    var userId = req.user.id
+
+    if (userId)
+      res.redirect('/v1/users/' + userId)
+    else
+      res.jsonp({}, 404)
+  })
+
   app.get('/v1/users/:userId', function(req, res) {
     models.User.findById(req.params.userId, function(err, user) {
       if (err) return res.jsonp({}, 422)
