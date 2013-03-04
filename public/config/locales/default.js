6f0e459c369f5b4f08f09f0e64f89d082348519a
epicmonkey <mail@epicmonkey.org>
2013-03-05 01:11:06 +0400

    Adds extra labels for stats localization.

--- public/config/locales/default.js
+++ public/config/locales/default.js
@@ -1,19 +1,27 @@
 Em.I18n.translations = {
   'title.title': 'Pepyatka',
-  'label.version': '0.0.4',
+
   'message.welcome': 'Seems like you\'ve just started using Pepyatka?',
+
   'label.signin': 'Sign in',
   'label.signup': 'Join Pepyatka',
   'label.settings': 'settings',
   'label.signout': 'sign out',
-  'button.next': 'Next &rarr;',
-  'button.previous': '&larr; Previous',
+  'label.version': '0.0.4',
   'label.add.image': 'Add image: ',
-  'button.post': 'Post',
   'label.cancel': 'Cancel',
-  'button.Search': 'Search',
   'label.comment': 'Comment',
   'label.like': 'Like',
   'label.unlike': 'Un-like',
-  'label.liked': 'liked this'
+  'label.liked': 'liked this',
+  'label.subscriptions': 'subscriptions'
+  'label.subscribers': 'subscribers'
+  'label.likes': 'likes'
+  'label.posts': 'posts'
+  'label.comments': 'comments'
+
+  'button.Search': 'search',
+  'button.post': 'Post',
+  'button.next': 'Next &rarr;',
+  'button.previous': '&larr; Previous'
 };
