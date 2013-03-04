117a71a01dcf77e68f10f808870748c7ac5ab5e5
Andrey Glazunov <Gvinar@yandex.ru>
2013-03-04 19:54:35 +0400

    Implemented white labelling

---
+++ public/config/locales/default.js
@@ -0,0 +1,18 @@
+Em.I18n.translations = {
+  'title.title': 'Pepyatka',
+  'label.version': '0.0.3',
+  'message.welcome': 'Seems like you\'ve just started using Pepyatka?',
+  'label.signin': 'Sign in',
+  'label.signup': 'Join Pepyatka',
+  'label.settings': 'settings',
+  'label.signout': 'sign out',
+  'button.next': 'Next &rarr;',
+  'button.previous': '&larr; Previous',
+  'label.add.image': 'Add image: ',
+  'button.post': 'Post',
+  'button.Search': 'Search',
+  'label.comment': 'Comment',
+  'label.like': 'Like',
+  'label.unlike': 'Un-like',
+  'label.liked': 'liked this'
+};
