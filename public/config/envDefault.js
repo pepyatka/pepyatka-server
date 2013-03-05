1cab8a5110975d2237236d7f25d752aff97f1be6
epicmonkey <mail@epicmonkey.org>
2013-03-05 22:00:21 +0400

    Removes escapted quote in js configDefault.

--- public/config/envDefault.js
+++ public/config/envDefault.js
@@ -3,5 +3,5 @@ Config.loadScripts = function(){
   var labellingFilePath = '/config/locales/default.js'
 
   $(document.body).append("<script src='" + labellingFilePath + "'></script>")
-  $(document.body).append("<script src='/js/app/app.js\'></script>")
+  $(document.body).append("<script src='/js/app/app.js'></script>")
 }
