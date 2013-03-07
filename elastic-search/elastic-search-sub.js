19f9b696fd5b7fce6a0ff610f84d62ed8be69340
Andrey Glazunov <Gvinar@yandex.ru>
2013-03-07 16:35:28 +0400

    Added indexation for add/remove like and delete post or comment

--- elastic-search/elastic-search-sub.js
+++ elastic-search/elastic-search-sub.js
@@ -3,72 +3,108 @@ var models = require('./../app/models')
   , searchClient = require('./elastic-search-client.js');
 
 exports.listen = function() {
-    var sub = redis.createClient();
+  var sub = redis.createClient();
 
-    sub.subscribe('newPost', 'destroyPost',
-        'newComment', 'destroyComment',
-        'newLike', 'removeLike' )
+  sub.subscribe('newPost', 'destroyPost',
+    'newComment', 'destroyComment',
+    'newLike', 'removeLike' )
 
-    sub.on('message', function(channel, msg) {
-        switch(channel) {
-//            case 'destroyPost':
-//                var data = JSON.parse(msg)
-//                var event = { postId: data.postId }
-//
-//                break
-//
-            case 'newPost':
-                var data = JSON.parse(msg);
-                models.Post.findById(data.postId, function(err, post) {
-                  models.Timeline.findById(data.timelineId, {}, function(err, timeline) {
-                    // TODO: workaround to index just one post
-                    if (post && timeline && timeline.name == 'Posts') {
-                      post.toJSON({ select: ['id', 'body', 'createdBy', 'attachments', 'comments', 'createdAt', 'updatedAt', 'likes', 'timelineId'],
-                          createdBy: { select: ['id', 'username'] },
-                          comments: { select: ['id', 'body', 'createdBy'],
-                            createdBy: { select: ['id', 'username'] } },
-                          likes: { select: ['id', 'username']}
-                        },
-                        function(err, json) {
-                          searchClient.indexElement('pepyatka', 'post', json);
-                        })
-                    }
-                  })
-                });
-                break;
+  sub.on('message', function(channel, msg) {
+    switch(channel) {
+      case 'destroyPost':
+        var data = JSON.parse(msg)
+        searchClient.deleteElement('pepyatka', 'post', data.postId)
+        break
 
-            case 'newComment':
-                var data = JSON.parse(msg)
-                models.Post.findById(data.postId, function(err, post) {
-                  if (post) {
-                    post.toJSON({ select: ['id', 'body', 'createdBy', 'attachments', 'comments', 'createdAt', 'updatedAt', 'likes', 'timelineId'],
-                        createdBy: { select: ['id', 'username'] },
-                        comments: { select: ['id', 'body', 'createdBy'],
-                          createdBy: { select: ['id', 'username'] } },
-                        likes: { select: ['id', 'username']}
-                      },
-                      function(err, json) {
-                        searchClient.updateElement('pepyatka', 'post', json);
-                      })
-                  }
+      case 'newPost':
+        var data = JSON.parse(msg);
+        models.Post.findById(data.postId, function(err, post) {
+          models.Timeline.findById(data.timelineId, {}, function(err, timeline) {
+            // TODO: workaround to index just one post
+            if (post && timeline && timeline.name == 'Posts') {
+              post.toJSON({ select: ['id', 'body', 'createdBy', 'attachments', 'comments', 'createdAt', 'updatedAt', 'likes', 'timelineId'],
+                  createdBy: { select: ['id', 'username'] },
+                  comments: { select: ['id', 'body', 'createdBy'],
+                    createdBy: { select: ['id', 'username'] } },
+                  likes: { select: ['id', 'username']}
+                },
+                function(err, json) {
+                  searchClient.indexElement('pepyatka', 'post', json);
                 })
-                break;
+            }
+          })
+        });
+        break
 
-//            case 'newLike':
-//                var data = JSON.parse(msg)
-//
-//                models.User.findById(data.userId, function(err, user) {
-//                    if (user) {
-//                        user.toJSON(function(err, json) {
-//
-//                        });
-//                break;
-//
-//            case 'removeLike':
-//                var data = JSON.parse(msg)
-//                var event = { userId: data.userId, postId: data.postId }
-//
-//                break
-        }
-    })
+      case 'newComment':
+          var data = JSON.parse(msg)
+          models.Post.findById(data.postId, function(err, post) {
+            if (!post) return
+
+            post.toJSON({ select: ['id', 'body', 'createdBy', 'attachments', 'comments', 'createdAt', 'updatedAt', 'likes', 'timelineId'],
+                createdBy: { select: ['id', 'username'] },
+                comments: { select: ['id', 'body', 'createdBy'],
+                  createdBy: { select: ['id', 'username'] } },
+                likes: { select: ['id', 'username']}
+              },
+              function(err, json) {
+                searchClient.updateElement('pepyatka', 'post', json);
+              })
+          })
+          break
+
+      case 'newLike':
+        var data = JSON.parse(msg)
+        models.Post.findById(data.postId, function(err, post) {
+          if (!post) return
+
+          post.toJSON({ select: ['id', 'body', 'createdBy', 'attachments', 'comments', 'createdAt', 'updatedAt', 'likes', 'timelineId'],
+              createdBy: { select: ['id', 'username'] },
+              comments: { select: ['id', 'body', 'createdBy'],
+                createdBy: { select: ['id', 'username'] } },
+              likes: { select: ['id', 'username']}
+            },
+            function(err, json) {
+              searchClient.updateElement('pepyatka', 'post', json);
+            })
+        })
+
+        break
+
+      case 'removeLike':
+        var data = JSON.parse(msg)
+        models.Post.findById(data.postId, function(err, post) {
+          if (!post) return
+
+          post.toJSON({ select: ['id', 'body', 'createdBy', 'attachments', 'comments', 'createdAt', 'updatedAt', 'likes', 'timelineId'],
+              createdBy: { select: ['id', 'username'] },
+              comments: { select: ['id', 'body', 'createdBy'],
+                createdBy: { select: ['id', 'username'] } },
+              likes: { select: ['id', 'username']}
+            },
+            function(err, json) {
+              searchClient.updateElement('pepyatka', 'post', json);
+            })
+        })
+
+        break
+
+      case 'destroyComment':
+        var data = JSON.parse(msg)
+        models.Post.findById(data.postId, function(err, post) {
+          if (!post) return
+
+          post.toJSON({ select: ['id', 'body', 'createdBy', 'attachments', 'comments', 'createdAt', 'updatedAt', 'likes', 'timelineId'],
+              createdBy: { select: ['id', 'username'] },
+              comments: { select: ['id', 'body', 'createdBy'],
+                createdBy: { select: ['id', 'username'] } },
+              likes: { select: ['id', 'username']}
+            },
+            function(err, json) {
+              searchClient.updateElement('pepyatka', 'post', json);
+            })
+        })
+        break
+    }
+  })
 }
