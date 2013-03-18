a5eee99a276ea2aa7d342e9a5bb7b3e4c659c780
Andrey Glazunov <Gvinar@yandex.ru>
2013-03-18 14:01:12 +0400

    Fixed issues with different data bases for elasticsearch

--- bin/reindexd.js
+++ bin/reindexd.js
@@ -1,1 +1,20 @@
-var elasticSearchReindexator = require('./../elastic-search/elastic-search-reindexator.js').startInspection();
+var express = require('express')
+  , app = express()
+  , db = require('../db').connect()
+  ,elasticSearchReindexator = require('./../elastic-search/elastic-search-reindexator.js');
+
+app.configure('development', function() {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.set('redisdb', 1);
+});
+
+app.configure('production', function(){
+  app.set('redisdb', 0);
+});
+
+app.configure('test', function(){
+  app.set('redisdb', 2);
+});
+db.select(app.get('redisdb'), function(err, res) {
+  elasticSearchReindexator.startInspection();
+})
--- bin/searchd.js
+++ bin/searchd.js
@@ -1,1 +1,20 @@
-var elasticSearch = require('./../elastic-search/elastic-search-sub.js').listen();
+var express = require('express')
+  , app = express()
+  , db = require('../db').connect()
+  , elasticSearch = require('./../elastic-search/elastic-search-sub.js');
+
+app.configure('development', function() {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.set('redisdb', 1);
+});
+
+app.configure('production', function(){
+  app.set('redisdb', 0);
+});
+
+app.configure('test', function(){
+  app.set('redisdb', 2);
+});
+db.select(app.get('redisdb'), function(err, res) {
+  elasticSearch.listen();
+})
