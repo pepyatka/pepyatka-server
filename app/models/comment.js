9a4d0666a2685927ec1e4f393813a6fda20a96e0
epicmonkey <mail@epicmonkey.org>
2013-03-20 23:08:03 +0400

    Fixes comments deletion.

--- app/models/comment.js
+++ app/models/comment.js
@@ -63,6 +63,8 @@ exports.addModel = function(db) {
                         callback(err, res)
                       }
                     })
+                  } else {
+                    callback(err, res)
                   }
                 } else {
                   callback(err, res)
