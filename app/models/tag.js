c8a81fc969cee477cf0365b392ba7976de629e8d
Andrey Glazunov <Gvinar@yandex.ru>
2013-04-02 17:05:46 +0400

    Added extract and update methods and changed diff method in Tag model

--- app/models/tag.js
+++ app/models/tag.js
@@ -1,5 +1,6 @@
 var uuid = require('node-uuid')
   , models = require('../models')
+  , async = require('async')
 
 exports.addModel = function(db) {
   function Tag(params) {
@@ -18,21 +19,55 @@ exports.addModel = function(db) {
     for (var tagName in object1) {
       comparisonTags.push(tagName)
       if (object2[tagName]) {
-        result[tagName] = object1[tagName] - object2[tagName]
+        result[tagName] = object2[tagName] - object1[tagName]
         continue
       }
 
-      result[tagName] = object1[tagName]
+      result[tagName] = object1[tagName] * -1
     }
 
     for (var tagName in object2) {
       if (comparisonTags.indexOf(tagName) != -1)
         continue
 
-      result[tagName] = object2[tagName] * -1
+      result[tagName] = object2[tagName]
     }
 
     callback(null, result)
+  }
+
+  Tag.extract = function(text, callback) {
+    var tags = text.match(/#[А-Яа-я\w]+/ig)
+    var result = {}
+    async.forEach(tags, function(tag, done) {
+      tag = tag.toLowerCase()
+      if (result[tag]) {
+        result[tag]++
+        done()
+      }
+
+      result[tag] = 1
+      done()
+    },
+    function(err) {
+      callback(err, result)
+    })
+  }
+
+  Tag.update = function(tagInfoObject, callback) {
+    var tags = []
+    for (var tag in tagInfoObject) {
+      tags.push({ tagName: tag, count: tagInfoObject[tag] })
+    }
+
+    async.forEach(tags, function(tag, done) {
+        db.zincrby('tags:everyone', tag.count, tag.tagName, function(err, res) {
+          done(err)
+        })
+      },
+      function(err) {
+        callback(err)
+      })
   }
   
   Tag.prototype = {
