eca344f612a73bf67bd99228d84b5007b6c4fd70
epicmonkey <mail@epicmonkey.org>
2013-04-09 22:41:49 +0400

    Bumps version to 0.0.7.

--- public/config/locales/default.js
+++ public/config/locales/default.js
@@ -7,7 +7,7 @@ Em.I18n.translations = {
   'label.signup': 'Join Pepyatka',
   'label.settings': 'settings',
   'label.signout': 'sign out',
-  'label.version': '0.0.6',
+  'label.version': '0.0.7',
   'label.add.image': 'Add image: ',
   'label.cancel': 'Cancel',
   'label.comment': 'Comment',
