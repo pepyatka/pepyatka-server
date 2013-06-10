040b48fbf282bd09b8d5d733a1fe61b4015e28c9
epicmonkey <mail@epicmonkey.org>
2013-06-11 01:33:46 +0400

    Subscribes to a post channel when we are on a post route.

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1820,8 +1820,9 @@ App.PostRoute = Ember.Route.extend({
   setupController: function(controller, model) {
     this.controllerFor('groups').set('content', App.Group.findAll())
     this.controllerFor('tags').set('content', App.Tag.findAll())
-
     controller.set('content', model);
+
+    this.controllerFor('comet').set('channel', model)
   }
 })
 
