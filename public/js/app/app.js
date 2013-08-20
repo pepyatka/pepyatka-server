c272b00795f9706ea702b83e8599ce1958f264f7
epicmonkey <mail@epicmonkey.org>
2013-08-21 01:29:36 +0400

    Shows To and Colon groups on Post page.

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1040,6 +1040,37 @@ App.PostView = Ember.View.extend({
   isEditFormVisible: false,
   currentUser: currentUser,
 
+  firstTwoGroups: function() {
+    var groups = this.get("controller.content.groups");
+    var post   = this.get("controller.content");
+
+    if (groups) {
+      if (groups.length === 1) {
+        return groups.filter(function(e) {
+          return e.username != post.get("createdBy.username");
+        });
+      }
+    }
+
+    return groups;
+  }.property("controller.content.groups"),
+
+  myFeedOnly: function() {
+    var groups = this.get("controller.content.groups");
+    var post   = this.get("controller.content");
+
+    return groups.length === 1 &&
+      groups[0].username == post.get("createdBy.username");
+  }.property("controller.content.groups"),
+
+  toOrColon: function() {
+    return this.get("controller.content.groups").length === 1;
+  }.property("controller.content.groups"),
+
+  colonOrBlank: function() {
+    return this.get("controller.content.groups").length > 1;
+  }.property("controller.content.groups"),
+
   toggleVisibility: function() {
     this.toggleProperty('isFormVisible');
   },
