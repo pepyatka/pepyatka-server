49e7bf4b390c6c95bd2f12a0569a4593aa2dc7cf
epicmonkey <mail@epicmonkey.org>
2013-08-25 00:24:24 +0400

    Fixes about view object name.

--- public/js/app/views/AboutView.js
+++ public/js/app/views/AboutView.js
@@ -1,6 +1,6 @@
 define(["app/app",
         "text!templates/aboutTemplate.handlebars"], function(App, tpl) {
-  App.TopView = Ember.View.extend({
+  App.AboutView = Ember.View.extend({
     templateName: 'about',
     template: Ember.Handlebars.compile(tpl)
   });
