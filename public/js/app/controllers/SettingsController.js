3f66d6a5851021d2e2b8e1f858efccbe7d1f56e8
Okasu <oka.sux@gmail.com>
2013-08-25 16:24:53 +0400

    Refactors settings page.

--- public/js/app/controllers/SettingsController.js
+++ public/js/app/controllers/SettingsController.js
@@ -1,14 +1,19 @@
 define(["app/app"], function(App) {
   App.SettingsController = Ember.ObjectController.extend({
-    save: function(params) {
-      var that = this
+    save: function() {
+      var that = this;
+      var params = {
+        screenName: this.get("info.screenName"),
+        email: this.get("info.email"),
+        receiveEmails: this.get("info.receiveEmails")
+      };
 
       App.User.save(params, {
         success: function(response) {
           that.set('content', response);
           that.transitionToRoute("home");
         }
-      })
+      });
     }
-  })
+  });
 });
--- public/js/app/views/SettingsView.js
+++ public/js/app/views/SettingsView.js
@@ -4,22 +4,9 @@ define(["app/app",
     templateName: 'settings',
     template: Ember.Handlebars.compile(tpl),
 
-    screenNameBinding: 'controller.content.info.screenName',
-    emailBinding: 'controller.content.info.email',
-    receiveEmailsBinding: 'controller.content.info.receiveEmails',
-
     receiveEmailsContent: [
       Ember.Object.create({name: "In real time", id: '0'}),
       Ember.Object.create({name: "Do not send",  id: '1'})
-    ],
-
-    save: function() {
-      var params = {
-        screenName: this.screenName,
-        email: this.email,
-        receiveEmails: (this.receiveEmails !== undefined) ? this.receiveEmails : null
-      }
-      this.get('controller').save(params)
-    }
-  })
+    ]
+  });
 });
