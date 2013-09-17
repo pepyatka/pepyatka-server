dad0efec815cab80574fffe29f3279bd17d3635d
epicmonkey <mail@epicmonkey.org>
2013-09-17 23:46:39 +0400

    Improves rss ids migration when it's been ran for the second time.

--- db/migrate/1379420648555-set-to-zset-rssids.js
+++ db/migrate/1379420648555-set-to-zset-rssids.js
@@ -7,16 +7,24 @@ var async = require("async");
 env.init(function(_,__,db) {
   db.smembers("rss_ids", function(_, ids) {
     db.del("rss_ids", function() {
+      var done = function() {
+        console.log("Updated all rss.")
+        process.exit(1);
+      }
+
+      if (ids.length === 0)
+        return done()
+
       async.forEach(ids, function(id, done) {
 
         RSS.find(id, function(_, rss) {
+          console.log("Updating " + id + ".")
           rss.addLookupId(function() {
             done();
           });
         });
-
       }, function() {
-        process.exit(1);
+        done()
       });
     });
   });
