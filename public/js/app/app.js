896f0010447af7ac56c81c3753d24e7187c901f9
epicmonkey <mail@epicmonkey.org>
2013-09-27 00:52:14 +0400

    Does not show debug info unless DEBUG variable is set.

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -11,10 +11,22 @@ define(["globals",
         "jquery.expander",
         "bootstrap.file-input"], function(globals, Ember){
   if (!globals.app) {
-    App = Ember.Application.create({
-      LOG_TRANSITIONS: true,
-      // LOG_TRANSITIONS_INTERNAL: true
-    });
+    var options = {}
+
+    if (typeof DEBUG !== 'undefined') {
+      Ember.LOG_BINDINGS = true;
+      options = {
+        // log when Ember generates a controller or a route from a generic class
+        LOG_ACTIVE_GENERATION: true,
+        // log when Ember looks up a template or a view
+        LOG_VIEW_LOOKUPS: true,
+
+        LOG_TRANSITIONS: true
+        // LOG_TRANSITIONS_INTERNAL: true
+      }
+    }
+
+    App = Ember.Application.create(options);
 
     // We need to delay initialization to load the rest of the application
     App.deferReadiness()
