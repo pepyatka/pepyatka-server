001b0c5273f31372b6b30ed8e2b064904583b795
epicmonkey <mail@epicmonkey.org>
2014-07-17 01:21:58 +0400

    Adds test that check limit option for timelines.

--- test/functional/timeline.js
+++ test/functional/timeline.js
@@ -326,6 +326,47 @@ describe('Timeline API', function() {
     })
   })
 
+  it('GET /v1/timeline?limit=1 should return one post', function(done) {
+    var posts = []
+    var length = 40
+
+    db.flushdb(function(err) {
+      models.User.findAnon(function(err, user) {
+        var bodies = []
+        for(var i = 0; i < length; i++) {
+          bodies.push('postBody-' + i.toString())
+        }
+
+        async.mapSeries(bodies, function(body, done) {
+          user.newPost({
+            body: body
+          }, function(err, post) {
+            done(err, post)
+          });
+        }, function(err, posts) {
+          async.forEachSeries(posts, function(post, callback) {
+            post.create(function(err) {
+              callback(err)
+            })
+          }, function(err) {
+            request(server)
+              .get('/v1/timeline?limit=1')
+              .expect('Content-Type', /json/)
+              .expect(200, function(err, res) {
+                assert.equal(err, null)
+
+                var jsonTimeline = res.body
+                assert.equal(jsonTimeline.posts.length, 1)
+                assert.equal(jsonTimeline.posts[0].body, 'postBody-39')
+
+                done()
+              })
+          })
+        })
+      })
+    })
+  })
+
   it('GET /v1/timeline/404-user should return 404', function(done) {
     request(server)
       .get('/v1/timeline/404-user')
