1e59a051d83b069ed32d722b1c17ea240d17391d
epicmonkey <mail@epicmonkey.org>
2014-07-22 14:32:11 +0400

    Improves way we work with browser auto-fill on signin form.

--- public/js/app/views/SigninView.js
+++ public/js/app/views/SigninView.js
@@ -4,8 +4,30 @@ define(["app/app",
     templateName: 'signin',
     template: Ember.Handlebars.compile(tpl),
 
+    didInsertElement: function() {
+      this.tick();
+    },
+
+
+    willDestroyElement: function() {
+      clearTimeout(this._timer);
+    },
+
+
     insertNewline: function() {
       this.triggerAction();
+    },
+
+    // we need this timer so can by-pass browser autofill
+    tick: function() {
+      var interval = 250
+      var self = this;
+
+      if (this.$()) {
+        this.set('username.value', this.$('#username').val())
+        this.set('password.value', this.$('#username').val())
+      }
+      setTimeout(function() { self.tick(); }, interval)
     }
   });
 });
