6a172758ab3e0f52545dffbd85c244be9b9e6937
epicmonkey <mail@epicmonkey.org>
2014-10-11 16:25:25 +0400

    Drops version route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,6 @@
 var models = require('../models');
 
 exports.addRoutes = function(app) {
-  app.get('/api/v1/version', function(req, res) {
-    res.json({status: 'All your base are belong to us!11',
-              version: '0.0.6'})
-  })
-
   app.use(function(req, res) {
     res.render('./home', {csrf_token: req.session._csrf})
   })
