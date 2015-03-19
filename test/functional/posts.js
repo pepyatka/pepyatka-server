1cfe44574bf7cb9ba5c8e6c74f609a73073fd78d
epicmonkey <mail@epicmonkey.org>
2015-03-20 01:50:48 +0300

    Tests like and unlike post.

--- test/functional/posts.js
+++ test/functional/posts.js
@@ -60,4 +60,146 @@ describe("PostsController", function() {
         })
     })
   })
+
+  describe('#like()', function() {
+    var authToken
+      , post
+
+    beforeEach(function(done) {
+      var user = {
+        username: 'Luna',
+        password: 'password'
+      }
+
+      request
+        .post(app.config.host + '/v1/users')
+        .send({ username: user.username, password: user.password })
+        .end(function(err, res) {
+          res.should.not.be.empty
+          res.body.should.not.be.empty
+          res.body.should.have.property('authToken')
+          authToken = res.body.authToken
+
+          var body = 'Post body'
+
+          request
+            .post(app.config.host + '/v1/posts')
+            .send({ post: { body: body }, authToken: authToken })
+            .end(function(err, res) {
+              res.body.should.not.be.empty
+              res.body.should.have.property('posts')
+              res.body.posts.should.have.property('body')
+              res.body.posts.body.should.eql(body)
+
+              post = res.body.posts
+
+              done()
+            })
+        })
+    })
+
+    it('should like post with a valid user', function(done) {
+      request
+        .post(app.config.host + '/v1/posts/' + post.id + '/like')
+        .send({ authToken: authToken })
+        .end(function(err, res) {
+          res.body.should.be.empty
+          $should.not.exist(err)
+
+          done()
+        })
+    })
+
+    it('should not like post with an invalid user', function(done) {
+      request
+        .post(app.config.host + '/v1/posts/' + post.id + '/like')
+        .end(function(err, res) {
+          err.should.not.be.empty
+          err.status.should.eql(401)
+          done()
+        })
+    })
+
+    it('should not like invalid post', function(done) {
+      request
+        .post(app.config.host + '/v1/posts/:id/like')
+        .send({ authToken: authToken })
+        .end(function(err, res) {
+          err.should.not.be.empty
+          err.status.should.eql(401)
+          done()
+        })
+    })
+  })
+
+  describe('#unlike()', function() {
+    var authToken
+      , post
+
+    beforeEach(function(done) {
+      var user = {
+        username: 'Luna',
+        password: 'password'
+      }
+
+      request
+        .post(app.config.host + '/v1/users')
+        .send({ username: user.username, password: user.password })
+        .end(function(err, res) {
+          res.should.not.be.empty
+          res.body.should.not.be.empty
+          res.body.should.have.property('authToken')
+          authToken = res.body.authToken
+
+          var body = 'Post body'
+
+          request
+            .post(app.config.host + '/v1/posts')
+            .send({ post: { body: body }, authToken: authToken })
+            .end(function(err, res) {
+              res.body.should.not.be.empty
+              res.body.should.have.property('posts')
+              res.body.posts.should.have.property('body')
+              res.body.posts.body.should.eql(body)
+
+              post = res.body.posts
+
+              done()
+            })
+        })
+    })
+
+    it('should unlike post with a valid user', function(done) {
+      request
+        .post(app.config.host + '/v1/posts/' + post.id + '/unlike')
+        .send({ authToken: authToken })
+        .end(function(err, res) {
+          res.body.should.be.empty
+          $should.not.exist(err)
+
+          done()
+        })
+    })
+
+    it('should not unlike post with an invalid user', function(done) {
+      request
+        .post(app.config.host + '/v1/posts/' + post.id + '/unlike')
+        .end(function(err, res) {
+          err.should.not.be.empty
+          err.status.should.eql(401)
+          done()
+        })
+    })
+
+    it('should not unlike invalid post', function(done) {
+      request
+        .post(app.config.host + '/v1/posts/:id/unlike')
+        .send({ authToken: authToken })
+        .end(function(err, res) {
+          err.should.not.be.empty
+          err.status.should.eql(401)
+          done()
+        })
+    })
+  })
 })
