329e52258f5328f23f538b2555b513b145720963
epicmonkey <mail@epicmonkey.org>
2015-03-22 00:05:08 +0300

    Allows cross-domain PUT and PATCH requests

--- config/initializers/origin.js
+++ config/initializers/origin.js
@@ -4,7 +4,8 @@ var config = require('./../config').load()
 
 exports.init = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', config.origin)
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Authentication-Token")
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Authentication-Token, Access-Control-Request-Method")
 
   return next()
 }
