2eccee25b83abccb62df1cef54bff619e526f8c9
Alex Olshansky <clbn@spotify.com>
2015-03-30 02:35:49 +0200

    Attachments: model, list of existing attachments
    * There is no attachment uploading yet
    * Boolean property 'isImage' only: no support for various file types (audio, video, PDF etc.)

--- app/serializers/v1/PostSerializer.js
+++ app/serializers/v1/PostSerializer.js
@@ -1,11 +1,13 @@
 var models = require("../../models")
   , Serializer = models.Serializer
   , UserSerializer = models.UserSerializer
+  , AttachmentSerializer = models.AttachmentSerializer
   , CommentSerializer = models.CommentSerializer
 
 exports.addSerializer = function() {
   return new Serializer("posts", {
-    select: ['id', 'body', 'createdBy', 'comments', 'createdAt', 'updatedAt', 'updatedAt', 'likes'],
+    select: ['id', 'body', 'attachments', 'createdBy', 'comments', 'createdAt', 'updatedAt', 'updatedAt', 'likes'],
+    attachments: { through: AttachmentSerializer, embed: true },
     createdBy: { through: UserSerializer, embed: true },
     comments: { through: CommentSerializer, embed: true },
     likes: { through: UserSerializer, embed: true }
