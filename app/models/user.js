3c2a6cd14c4139924b909219b574c7ae536ad895
Alexey Zakhlestin <indeyets@gmail.com>
2015-04-14 20:42:12 +0300

    missing dependency (after rebase)

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var Promise = require('bluebird')
+  , crypto = Promise.promisifyAll(require('crypto'))
   , uuid = require('uuid')
   , config = require('../../config/config').load()
   , inherits = require("util").inherits
