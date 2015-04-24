d11e878bcb40e636f8f6970c104629b70c14a478
Alexey Zakhlestin <indeyets@gmail.com>
2015-04-25 01:51:46 +0300

    Changed texts of authentication errors
    Now they match those of Friendfeed

--- config/initializers/passport.js
+++ config/initializers/passport.js
@@ -11,18 +11,23 @@ exports.init = function(passport) {
   }, function(username, clearPassword, done) {
     models.User.findByUsername(username)
       .then(function(user) {
-        if (!user)
-          return done(null, false, { message: 'Incorrect username.'})
+        if (!user) {
+          // db inconsistency. got id, but didn't find object
+          return done({ message: "We could not find the nickname you provided." })
+        }
 
         user.validPassword(clearPassword)
           .then(function(valid) {
             if (valid)
               return done(null, user)
             else
-              return done(null, false, { message: 'Incorrect password.'})
+              return done({ message: "The password you provided does not match the password in our system." })
           })
       })
-      .catch(function(e) { done(e, false) })
+      .catch(function(e) {
+        // didn't find id by username
+        return done({ message: "We could not find the nickname you provided." })
+      })
   }))
 }
 
