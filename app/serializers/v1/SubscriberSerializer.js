c5fbd1256772d40314cb66f99cfa4acebf5c7605
epicmonkey <mail@epicmonkey.org>
2015-04-29 01:38:01 +0300

    Serializes userpic for Subscriber (this is a simplified User).
    Might be eventually we'll merge them into one.

--- app/serializers/v1/SubscriberSerializer.js
+++ app/serializers/v1/SubscriberSerializer.js
@@ -2,6 +2,7 @@ var Serializer = require("../../models").Serializer
 
 exports.addSerializer = function() {
   return new Serializer("subscribers", {
-    select: ['id', 'username', 'screenName', 'type', 'updatedAt', 'createdAt']
+    select: ['id', 'username', 'screenName', 'type', 'updatedAt', 'createdAt',
+             'profilePictureLargeUrl', 'profilePictureMediumUrl']
   })
 }
