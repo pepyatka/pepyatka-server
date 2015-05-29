993f149587a7b81b8d25fe3383488df211214638
epicmonkey <mail@epicmonkey.org>
2015-05-29 18:10:13 +0300

    Serializes subscriptions to a group.

--- app/serializers/v1/GroupSerializer.js
+++ app/serializers/v1/GroupSerializer.js
@@ -1,9 +1,11 @@
 var models = require("../../models")
   , Serializer = models.Serializer
   , UserSerializer = models.UserSerializer
+  , SubscriptionSerializer = models.SubscriptionSerializer
 
 exports.addSerializer = function() {
   return new Serializer('groups', {
-    select: ['id', 'username', 'type', 'screenName', 'isPrivate', 'administratorIds']
+    select: ['id', 'username', 'type', 'screenName', 'isPrivate', 'administratorIds', 'timelines'],
+    timelines: { through: SubscriptionSerializer, embed: true }
   })
 }
