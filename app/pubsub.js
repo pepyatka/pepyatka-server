93b3bf6fa5cd6dc6fa2ea744f5aa733bbbb6b19d
Alexey Zakhlestin <indeyets@gmail.com>
2015-07-03 01:24:36 +0300

    converted pubsub to es6-class

--- app/pubsub.js
+++ app/pubsub.js
@@ -3,139 +3,138 @@ import models from './models'
 exports.init = function(database) {
   "use strict";
 
-  var pubSub = function() {
-  }
+  class pubSub {
+    static async newPost(postId) {
+      var post = await models.Post.findById(postId)
+      var timelineIds = await post.getSubscribedTimelineIds()
 
-  pubSub.newPost = async function(postId) {
-    var post = await models.Post.findById(postId)
-    var timelineIds = await post.getSubscribedTimelineIds()
+      var promises = timelineIds.map(async function(timelineId) {
+        let jsonedPost = JSON.stringify({ postId: postId, timelineId: timelineId })
+        await database.publishAsync('post:new', jsonedPost)
+      })
 
-    var promises = timelineIds.map(async function(timelineId) {
-      let jsonedPost = JSON.stringify({ postId: postId, timelineId: timelineId })
-      await database.publishAsync('post:new', jsonedPost)
-    })
+      await* promises
+    }
 
-    await* promises
-  }
+    static async destroyPost(postId) {
+      var post = await models.Post.findById(postId)
+      var timelineIds = await post.getSubscribedTimelineIds()
 
-  pubSub.destroyPost = async function(postId) {
-    var post = await models.Post.findById(postId)
-    var timelineIds = await post.getSubscribedTimelineIds()
+      var promises = timelineIds.map(async function(timelineId) {
+        let jsonedPost = JSON.stringify({ postId: postId, timelineId: timelineId })
+        await database.publishAsync('post:destroy', jsonedPost)
+      })
 
-    var promises = timelineIds.map(async function(timelineId) {
-      let jsonedPost = JSON.stringify({ postId: postId, timelineId: timelineId })
-      await database.publishAsync('post:destroy', jsonedPost)
-    })
+      await* promises
+    }
 
-    await* promises
-  }
+    static async updatePost(postId) {
+      var post = await models.Post.findById(postId)
+      var timelineIds = await post.getSubscribedTimelineIds()
 
-  pubSub.updatePost = async function(postId) {
-    var post = await models.Post.findById(postId)
-    var timelineIds = await post.getSubscribedTimelineIds()
+      var promises = timelineIds.map(async function(timelineId) {
+        let jsonedPost = JSON.stringify({ postId: postId, timelineId: timelineId })
+        await database.publishAsync('post:update', jsonedPost)
+      })
 
-    var promises = timelineIds.map(async function(timelineId) {
-      let jsonedPost = JSON.stringify({ postId: postId, timelineId: timelineId })
-      await database.publishAsync('post:update', jsonedPost)
-    })
+      await* promises
+    }
 
-    await* promises
-  }
+    static async newComment(commentId) {
+      var comment = await models.Comment.findById(commentId)
+      var post = await comment.getPost()
+      var timelines = await post.getCommentsFriendOfFriendTimelines(comment.userId)
 
-  pubSub.newComment = async function(commentId) {
-    var comment = await models.Comment.findById(commentId)
-    var post = await comment.getPost()
-    var timelines = await post.getCommentsFriendOfFriendTimelines(comment.userId)
+      var promises = timelines.map(async function(timeline) {
+        let isBanned = await post.isBannedFor(timeline.userId)
+        let isHidden = await post.isHiddenIn(timeline.id)
 
-    var promises = timelines.map(async function(timeline) {
-      let isBanned = await post.isBannedFor(timeline.userId)
-      let isHidden = await post.isHiddenIn(timeline.id)
+        if (!isHidden && !isBanned) {
+          let payload = JSON.stringify({ timelineId: timeline.id, commentId: commentId })
+          await database.publishAsync('comment:new', payload)
+        }
+      })
 
-      if (!isHidden && !isBanned) {
-        let payload = JSON.stringify({ timelineId: timeline.id, commentId: commentId })
-        await database.publishAsync('comment:new', payload)
-      }
-    })
+      await* promises
 
-    await* promises
+      let payload = JSON.stringify({ postId: post.id, commentId: commentId })
+      await database.publishAsync('comment:new', payload)
+    }
 
-    let payload = JSON.stringify({ postId: post.id, commentId: commentId })
-    await database.publishAsync('comment:new', payload)
-  }
+    static async destroyComment(commentId) {
+      var comment = await models.Comment.findById(commentId)
+      var post = await comment.getPost()
 
-  pubSub.destroyComment = async function(commentId) {
-    var comment = await models.Comment.findById(commentId)
-    var post = await comment.getPost()
+      let payload = JSON.stringify({ postId: post.id, commentId: commentId })
+      await database.publishAsync('comment:destroy', payload)
+    }
 
-    let payload = JSON.stringify({ postId: post.id, commentId: commentId })
-    await database.publishAsync('comment:destroy', payload)
-  }
+    static async updateComment(commentId) {
+      var comment = await models.Comment.findById(commentId)
+      var post = await comment.getPost()
 
-  pubSub.updateComment = async function(commentId) {
-    var comment = await models.Comment.findById(commentId)
-    var post = await comment.getPost()
-
-    let payload = JSON.stringify({ postId: post.id, commentId: commentId })
-    await database.publishAsync('comment:update', payload)
-
-    var timelineIds = await post.getSubscribedTimelineIds()
-    var promises = timelineIds.map(async function(timelineId) {
-      let payload = JSON.stringify({ timelineId: timelineId, commentId: commentId })
+      let payload = JSON.stringify({ postId: post.id, commentId: commentId })
       await database.publishAsync('comment:update', payload)
-    })
 
-    await* promises
-  }
+      var timelineIds = await post.getSubscribedTimelineIds()
+      var promises = timelineIds.map(async function(timelineId) {
+        let payload = JSON.stringify({ timelineId: timelineId, commentId: commentId })
+        await database.publishAsync('comment:update', payload)
+      })
 
-  pubSub.newLike = async function(postId, userId) {
-    var post = await models.Post.findById(postId)
-    var timelines = await post.getLikesFriendOfFriendTimelines(userId)
+      await* promises
+    }
 
-    var promises = timelines.map(async function(timeline) {
-      var isBanned = await post.isBannedFor(timeline.userId)
-      var isHidden = await post.isHiddenIn(timeline.id)
+    static async newLike(postId, userId) {
+      var post = await models.Post.findById(postId)
+      var timelines = await post.getLikesFriendOfFriendTimelines(userId)
 
-      if (!isHidden && !isBanned) {
-        let payload = JSON.stringify({ timelineId: timeline.id, userId: userId, postId: postId })
-        await database.publishAsync('like:new', payload)
-      }
-    })
+      var promises = timelines.map(async function(timeline) {
+        var isBanned = await post.isBannedFor(timeline.userId)
+        var isHidden = await post.isHiddenIn(timeline.id)
 
-    await* promises
+        if (!isHidden && !isBanned) {
+          let payload = JSON.stringify({ timelineId: timeline.id, userId: userId, postId: postId })
+          await database.publishAsync('like:new', payload)
+        }
+      })
 
-    let payload = JSON.stringify({ userId: userId, postId: postId })
-    await database.publishAsync('like:new', payload)
-  }
+      await* promises
 
-  pubSub.removeLike = async function(postId, userId) {
-    var post = await models.Post.findById(postId)
-    var timelineIds = await post.getSubscribedTimelineIds()
+      let payload = JSON.stringify({ userId: userId, postId: postId })
+      await database.publishAsync('like:new', payload)
+    }
 
-    var promises = timelineIds.map(async function(timelineId) {
-      let payload = JSON.stringify({ timelineId: timelineId, userId: userId, postId: postId })
+    static async removeLike(postId, userId) {
+      var post = await models.Post.findById(postId)
+      var timelineIds = await post.getSubscribedTimelineIds()
+
+      var promises = timelineIds.map(async function(timelineId) {
+        let payload = JSON.stringify({ timelineId: timelineId, userId: userId, postId: postId })
+        await database.publishAsync('like:remove', payload)
+      })
+
+      await* promises
+
+      let payload = JSON.stringify({ userId: userId, postId: postId })
       await database.publishAsync('like:remove', payload)
-    })
+    }
 
-    await* promises
+    static async hidePost(userId, postId) {
+      var user = await models.User.findById(userId)
+      var riverOfNewsId = await user.getRiverOfNewsTimelineId()
 
-    let payload = JSON.stringify({ userId: userId, postId: postId })
-    await database.publishAsync('like:remove', payload)
-  }
+      var payload = JSON.stringify({ timelineId: riverOfNewsId, postId: postId })
+      await database.publishAsync('post:hide', payload)
+    }
 
-  pubSub.hidePost = async function(userId, postId) {
-    var user = await models.User.findById(userId)
-    var riverOfNewsId = await user.getRiverOfNewsTimelineId()
+    static async unhidePost(userId, postId) {
+      var user = await models.User.findById(userId)
+      var riverOfNewsId = await user.getRiverOfNewsTimelineId()
 
-    var payload = JSON.stringify({ timelineId: riverOfNewsId, postId: postId })
-    await database.publishAsync('post:hide', payload)
-  }
-
-  pubSub.unhidePost = async function(userId, postId) {
-    var user = await models.User.findById(userId)
-    var riverOfNewsId = await user.getRiverOfNewsTimelineId()
-
-    var payload = JSON.stringify({ timelineId: riverOfNewsId, postId: postId })
-    await database.publishAsync('post:unhide', payload)
+      var payload = JSON.stringify({ timelineId: riverOfNewsId, postId: postId })
+      await database.publishAsync('post:unhide', payload)
+    }
   }
 
   return pubSub
