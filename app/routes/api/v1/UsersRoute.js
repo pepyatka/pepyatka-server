7e80154a487708bf627da456795534223df69e80
epicmonkey <mail@epicmonkey.org>
2015-07-24 12:09:35 +0300

    Nitpicking.

--- app/routes/api/v1/UsersRoute.js
+++ app/routes/api/v1/UsersRoute.js
@@ -5,9 +5,9 @@ var UsersController = require('../../../controllers').UsersController
 
 exports.addRoutes = function(app) {
   app.post('/v1/users',                          UsersController.create)
-  app.post( '/v1/users/acceptRequest/:username', UsersController.acceptRequest)
-  app.post( '/v1/users/rejectRequest/:username', UsersController.rejectRequest)
-  app.post( '/v1/users/:username/sendRequest',   UsersController.sendRequest)
+  app.post('/v1/users/acceptRequest/:username',  UsersController.acceptRequest)
+  app.post('/v1/users/rejectRequest/:username',  UsersController.rejectRequest)
+  app.post('/v1/users/:username/sendRequest',    UsersController.sendRequest)
   app.get( '/v1/users/whoami',                   UsersController.whoami)
   app.get( '/v1/users/:username',                UsersController.show)
   app.put( '/v1/users/updatePassword',           UsersController.updatePassword)
