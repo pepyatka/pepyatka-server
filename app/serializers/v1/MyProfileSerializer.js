a3f56c0572604c84e4a286d2a9ba6869558b7dbc
epicmonkey <mail@epicmonkey.org>
2015-08-04 10:59:45 +0300

    Serializes admins for /whoami request.

--- app/serializers/v1/MyProfileSerializer.js
+++ app/serializers/v1/MyProfileSerializer.js
@@ -3,16 +3,19 @@ var Serializer = require("../../models").Serializer
   , SubscriptionSerializer = models.SubscriptionSerializer
   , SubscriptionRequestSerializer = models.SubscriptionRequestSerializer
   , SubscriberSerializer = models.SubscriberSerializer
+  , AdminSerializer = models.AdminSerializer
 
 exports.addSerializer = function() {
   return new Serializer('users', {
     select: ['id', 'username', 'type', 'screenName', 'email', 'statistics',
              'subscriptions', 'profilePictureLargeUrl', 'profilePictureMediumUrl',
              'banIds', 'subscribers', 'isPrivate', 'pendingSubscriptionRequests',
-             'subscriptionRequests'],
+             'subscriptionRequests',
+             'administrators'],
     subscriptions: { through: SubscriptionSerializer, embed: true },
     subscribers: { through: SubscriberSerializer },
     pendingSubscriptionRequests: { through: SubscriptionRequestSerializer, embed: true },
-    subscriptionRequests: { through: SubscriptionRequestSerializer, embed: true }    
+    subscriptionRequests: { through: SubscriptionRequestSerializer, embed: true },
+    administrators: { through: AdminSerializer, embed: true }
   })
 }
