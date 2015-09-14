858a9e1db9360d495d275e1673b72ff95a28d228
Alex Olshansky <i@creagenics.com>
2015-09-14 18:23:04 +0200

    Complete bookmarklet implementation

--- app/controllers/api/v1/BookmarkletController.js
+++ app/controllers/api/v1/BookmarkletController.js
@@ -6,105 +6,93 @@ import url from 'url'
 import path from 'path'
 import crypto from 'crypto'
 import fs from 'fs'
-import http from 'http'
+import request from 'request'
+import Promise from 'bluebird'
 
 var config = config_loader.load()
 
 exports.addController = function(app) {
   class BookmarkletController {
     static async create(req, res) {
-     try {
-      var downloadFile = async function(imageUrl) {
-        var p = url.parse(imageUrl)
-        var options = {
-          host: p.host,
-          path: p.pathname
-        }
-
-        var ext = path.extname(p.pathname).split('.')
-        ext = ext[ext.length - 1]
-
-        var filename = 'pepyatka' + crypto.randomBytes(4).readUInt32LE(0) + 'tmp.' + ext
-        var filepath = '/tmp/' + filename
-        var file = fs.createWriteStream(filepath)
-        var image = ""
-        var files = []
-
-        http.get(options, function(response) {
-          var request = this
-          var contentLength = response.headers['content-length']
-          var maxLength = 100000
-
-          if (contentLength > maxLength) {
-            file.end()
-            request.abort()
-            return null
+      try {
+        var getAttachments = async function(imageUrl) {
+          if (!imageUrl) {
+            return []
           }
 
-          response.on('data', function (chunk) {
-            file.write(chunk)
-            image += chunk
+          var p = url.parse(imageUrl)
 
-            if (image.length > maxLength) {
-              file.end()
-              request.abort()
-              return null
-            }
-          })
+          var ext = path.extname(p.pathname).split('.')
+          ext = ext[ext.length - 1]
 
-          response.on('end', function() {
-            // req.user.newAttachment({ file: file })
-            //   .then(function(newAttachment) {
-            //     return newAttachment.create()
-            //   })
-            //   .then(function(newAttachment) {
-            //     new AttachmentSerializer(newAttachment).toJSON(function(err, json) {
-            //       res.jsonp(json)
-            //     })
-            //   })
+          var originalFileName = p.pathname.split('/').pop()
 
-            // files['file-0'] = {
-            //   path: filepath,
-            //   name: path.basename(filename)
-            // }
+          var fileName = 'pepyatka' + crypto.randomBytes(4).readUInt32LE(0) + 'tmp.' + ext
+          var filePath = '/tmp/' + fileName
+          var fileType;
 
-            file.end()
+          Promise.promisifyAll(request)
+          Promise.promisifyAll(fs)
 
-            return []
-          })
-        }).on('error', function(err) {
-          file.end()
-          return null
-        }).end()
-      }
+          let attachmentIds = await request.getAsync({ url: imageUrl, encoding: null })
+            .spread(function(response, body) {
+              fileType = response.headers['content-type']
+              return fs.writeFileAsync(filePath, body)
+            })
+            .then(function() {
+              return fs.statAsync(filePath)
+            })
+            .then(function(stats) {
+              let file = {
+                name: originalFileName,
+                size: stats.size,
+                type: fileType,
+                path: filePath
+              }
 
-      if (!req.user)
-        return res.status(401).jsonp({ err: 'Not found' })
+              return req.user.newAttachment({ file: file })
+                .then(function(newAttachment) {
+                  return newAttachment.create()
+                })
+                .then(function(newAttachment) {
+                  return [newAttachment.id]
+                })
+            })
 
-      // let files = await downloadFile(req.body.image)
-      let newPost = await req.user.newPost({
-        body: req.body.title
-        // attachments: files
-      })
+          return attachmentIds
+        }
 
-      let post = await newPost.create()
+        if (!req.user) {
+          return res.status(401).jsonp({err: 'Not found'})
+        }
 
-      if (req.body.comment) {
-        var newComment = await req.user.newComment({
-          body: req.body.comment,
-          postId: post.id
+        // Download image and create attachment
+        let attachments = await getAttachments(req.body.image)
+
+        // Create post
+        let newPost = await req.user.newPost({
+          body: req.body.title,
+          attachments: attachments
         })
+        let post = await newPost.create()
 
-        let comment = await newComment.create()
+        // Create comment
+        if (req.body.comment) {
+          var newComment = await req.user.newComment({
+            body: req.body.comment,
+            postId: post.id
+          })
 
-        res.jsonp({})
-      } else {
-        res.jsonp({})
+          let comment = await newComment.create()
+        }
+
+        // Send response with the created post
+        let json = await new PostSerializer(newPost).promiseToJSON()
+        res.jsonp(json)
+      } catch(e) {
+        console.log(e)
+        console.log(e.stack)
       }
-     } catch(e) {
-       console.log(e)
-       console.log(e.stack)
-     }
     }
   }
 
