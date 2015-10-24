ae19f68aa1f04bbb3ff922fc0b4d608a4f49143d
Alexey Zakhlestin <indeyets@gmail.com>
2015-10-24 07:27:31 +0300

    idiomatic implementation of mkKey

--- app/support/models.js
+++ app/support/models.js
@@ -1,13 +1,9 @@
 "use strict";
 
-var _ = require("lodash")
-
 var sep = ":"
 
 exports.mkKey = function(keys) {
-  return _.reduce(keys, function(acc, x) {
-    return acc + sep + x
-  })
+  return keys.join(sep)
 }
 
 
